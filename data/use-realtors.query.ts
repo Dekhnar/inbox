8bb4cd558e6ab81a7bf409c0b98cb66d54a2ce1f
Dekhnar <jessy95link@outlook.fr>
2021-06-11 10:30:05 +0200

    fix

--- data/use-realtors.query.ts
+++ data/use-realtors.query.ts
@@ -1,11 +1,14 @@
 import { Realtor, RealtorsService } from "@api";
 import { useQuery } from "react-query";
 
+export const fetchRealtors = async () => {
+  const reponse = await RealtorsService.getRealtors();
+  return reponse as Realtor[];
+
+}
+
 const useRealtorsQuery = () => {
-  return useQuery<Realtor[], Error>(`RealtorsService.getRealtors`, async () => {
-    const reponse = await RealtorsService.getRealtors();
-    return reponse as Realtor[];
-  });
+  return useQuery<Realtor[], Error>(`RealtorsService.getRealtors`, fetchRealtors);
 };
 
 export default useRealtorsQuery;
